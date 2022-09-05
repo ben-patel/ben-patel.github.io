de2668989142db87d789989eb22e330fd9464bd7
Ben Patel <97421407+ben-patel@users.noreply.github.com>
2022-09-05 15:52:08 +1200

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const port = process.env.PORT || 3000;
 
+var playerMap = new Map();
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
@@ -12,17 +14,24 @@ io.on('connection', (socket) => {
     io.emit('buy input', msg);
   });
 
-  socket.on('stop', msg => {
-    io.emit('stop', msg);
+  socket.on('real', msg => {
+    io.emit('real', msg);
   });
+
+  socket.on('profitUpdate', (profit,user) => {
+    playerMap[user] = profit;
+    io.emit('user', playerMap);
+  })
 
   socket.on('sell input', msg => {
     io.emit('sell input', msg);
   });
 
   socket.on('username', msg => {
+    playerMap[msg] = Number(0);
     io.emit('username', msg);
   });
+
   
   socket.on("topicEvent", message => {
     io.emit("topicEvent", message);
